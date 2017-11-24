ae81249a8edaf2a35493416d555cf2cfb663d710
aaronfox <aaron.m.fox3@gmail.com>
2017-11-23 22:30:20 -0500

    initial game commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is last. Needed to make sure offset is calculated first
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D player;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called for physics stuff
+	void FixedUpdate () {
+        
+	}
+
+    public void Launch()
+    {
+        Vector2 movement = new Vector2(30, 30);
+        player.AddForce(movement * speed);
+    }
+}
