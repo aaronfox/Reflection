a8dcfa8e124e086c5367956e36407a848eee97cd
aaronfox <aaron.m.fox3@gmail.com>
2017-11-23 23:35:15 -0500

    added colliding walls and physics 2D bouncy materials

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour {
     public float speed;
 
     private Rigidbody2D player;
+    private float xDir = 0, yDir = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -15,12 +16,23 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called for physics stuff
 	void FixedUpdate () {
-        
 	}
 
     public void Launch()
     {
-        Vector2 movement = new Vector2(30, 30);
-        player.AddForce(movement * speed);
+        player.AddForce(new Vector2(30, 30) * speed);
+    }
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        if(coll.gameObject.CompareTag("Wall"))
+        {
+            FlipVector();
+        }
+    }
+
+    void FlipVector()
+    {
+        //player.transform.Rotate(new Vector)
     }
 }
